ebb7814db7a1508d1d323e64f49378385ecf7c89
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-04-09 21:27:44 +0530

    Loops in Java

---
+++ src/For_loops.java
@@ -0,0 +1,23 @@
+package src;
+
+public class For_loops {
+    public static void main(String[] args) {
+//        for(int i = 0; i<=10; i++){
+//            System.out.println(i);
+//        }
+
+        // 2n = Even Numbers = 0, 2, 4 , 6, 8;
+        //2n+1 = Odd Numbers;
+//        int n = 3;
+//        for (int i = 0 ; i<n; i++){
+//            System.out.println(2*i+1);
+//        }
+
+
+        //Decrementing for loops in java
+
+        for (int i =5; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
