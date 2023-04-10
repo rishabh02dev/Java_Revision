db6cb192ef227ffb06918795863f8877420ae02a
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-04-10 20:12:15 +0530

    Arrays in java

--- src/Practice_Set_5.java
+++ src/Practice_Set_5.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.Scanner;
+
 public class Practice_Set_5 {
     public static void main(String[] args) {
         //practice problem 1
@@ -13,11 +15,52 @@ public class Practice_Set_5 {
 
         //Practice Problem 2
 
-        int sum =0;
-        int n=3;
-        for (int i=0; i<n; i++){
+//        int sum = 0;
+//        int n=6;
+//        for (int i=0; i<n; i++){
+//            sum = sum + (2*i);
+//        }
+//        System.out.println("Sum of even numbers is:  ");
+//        System.out.println(sum);
 
-        }
+
+
+        //Practice problem 3
+
+//        int n =5;
+//        for (int i = i ; i<=10;i++){
+//            System.out.printf("%d X %d = %d\n", n, i, n*i);
+//        }
+
+        //Practice Problem 4
+//
+//        int n =5;
+//        for (int i = 10 ; i>=1;i--){
+//            System.out.printf("%d X %d = %d\n", n, i, n*i);
+//        }
+
+
+        //Practice Problem 6
+
+//        int n =3;
+//      int factorial = 1;
+//      int i =1;
+//      while(i<=n){
+//          factorial = factorial*1;
+//          i++
+//      }
+
+        //Practice Problem 9
+//
+//        int n =5;
+//        int sum =0;
+//         for (int i = 1 ; i<=10;i++){
+//            sum = sum+8*i;
+//      }
+
+
+
+
 
 
     }
