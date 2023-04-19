b5c36e7159fefa771fdf5ec4518a726068ecb3c7
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-04-20 01:52:13 +0530

    Function Overloading

--- src/Method_Overloading.java
+++ src/Method_Overloading.java
@@ -5,6 +5,18 @@ public class Method_Overloading {
           a = 98;
     }
 
+    static void foo(){
+        System.out.println("Good Morning Brother!!");
+    }
+
+    static void foo(int a){
+        System.out.println("Good Morning " + a + " Bro!!");
+    }
+
+    static void foo(int a , int b){
+        System.out.println("Good morning " + a + "Bro !!" + b);
+    }
+
     static void change2(int [] arr){
           arr[0] = 98;
     }
@@ -18,10 +30,20 @@ public class Method_Overloading {
 //        System.out.println("The value of x after running change is  " + x);
         //Case 2 : It is the change in the array
 
-        int [] marks = {52, 73, 77, 89, 94};
-        change2(marks);
-        System.out.println("The value of x after running change is" + marks[0]);
+//        int [] marks = {52, 73, 77, 89, 94};
+//        change2(marks);
+//        System.out.println("The value of x after running change is" + marks[0]);
 
         //In arrays the refernce went there so it gets changed
+
+        //Method Overloading
+
+        foo();
+        foo(300);
+        foo(12, 14);
+
+        //arguements are the acutal values whereas parameters the variables
+        // which are made inside the method itself
+
     }
 }
