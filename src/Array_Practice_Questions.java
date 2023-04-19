7cf60c4fb98941087a2b9c70b3f301fad516f6d8
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-04-20 00:07:01 +0530

    Arrays questions

--- src/Array_Practice_Questions.java
+++ src/Array_Practice_Questions.java
@@ -2,6 +2,56 @@ package src;
 
 public class Array_Practice_Questions {
     public static void main(String[] args) {
-        
-    }
+        //Problem Number 1
+//         float [] numbers = new float[5];
+//         numbers[0] = 12.2f;
+//        numbers[1] = 12.2f;
+//        numbers[2] = 12.2f;
+//        numbers[3] = 12.2f;
+//        numbers[4] = 12.4f;
+//        System.out.println("The sum of arrays of 5 floats is");
+//        float sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
+//        System.out.println(sum);
+
+//        float [] marks = {12.2f , 12.2f , 12.3f , 12.5f, 12.4f};
+//        float sum = 0;
+//        for (float element: marks){
+//            sum = sum + element;
+//        }
+//        System.out.println(sum);
+
+        // Problem Number 2
+//        float [] marks = {12.2f , 12.2f , 12.3f , 12.5f, 12.4f};
+//        float num = 122.5f;
+//        boolean isInArray = false;
+//        for (float element:marks){
+//            if(num == element){
+//                isInArray = true;
+//                break;
+//            }
+//        }
+//        if(isInArray){
+//            System.out.println("The value is present in the array ");
+//        }
+//        else{
+//            System.out.println("The value is not present in the array");
+//        }
+
+
+        //Problem number 3
+//
+//        float [] marks = {12.2f , 12.2f , 12.3f , 12.5f, 12.4f};
+//        float sum = 0;
+//        for (float element: marks){
+//            sum = sum + element;
+//        }
+//         System.out.println("The value of average marks is " + sum/marks.length );
+//
+
+        //Practice Problem 4
+
+
+
+
+}
 }
