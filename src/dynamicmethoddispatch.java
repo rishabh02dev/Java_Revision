6c360e8774f767f6e7e7fc8963736d6c3a765b7f
kumkumshukla631@gmail.com <kumkumshukla631@gmail.com>
2023-05-07 13:00:18 +0530

    Practice problems on Dynamic class dispatch

--- src/dynamicmethoddispatch.java
+++ src/dynamicmethoddispatch.java
@@ -1,4 +1,32 @@
 package src;
 
+class Phone {
+    public void greet(){
+        System.out.println("Good Morning");
+    }
+    public void on(){
+        System.out.println("Turning on phone");
+    }
+}
+
+class SmartPhone extends Phone {
+    public void swagat(){
+        System.out.println("Apka swagat haii");
+    }
+    public void on(){
+        System.out.println("Turning on Smartphone");
+    }
+}
 public class dynamicmethoddispatch {
+    public static void main(String[] args) {
+//        Phone obj = new Phone();
+//        SmartPhone smobj = new SmartPhone();
+//        obj.name();
+
+        Phone obj = new SmartPhone(); //yes it is allowed
+
+        obj.greet();
+        obj.on(); 
+
+    }
 }
